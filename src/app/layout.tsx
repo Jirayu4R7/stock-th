eee026f999bac3ccfc4994bc5c6e0bd8d796b5c0
Jirayu4R7 <Jirayu4R7@users.noreply.gitlab.com>
2024-03-16 12:18:38 +0700

    Add style.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/themes/theme";
 import { Container } from "@mui/material";
+import "./index.css";
 
 export default function RootLayout({
   children,
