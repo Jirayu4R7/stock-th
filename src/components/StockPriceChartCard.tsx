982eee947bbb261406ca5af6b7f4302bd449a8cd
Jirayu4R7 <Jirayu4R7@users.noreply.gitlab.com>
2024-07-16 11:32:19 +0700

    fix: price chart is null.

--- src/components/StockPriceChartCard.tsx
+++ src/components/StockPriceChartCard.tsx
@@ -1,4 +1,4 @@
-import LineChartComponent from "@/components/LineChartComponent";
+import LineChartComponent, { DataPoint } from "@/components/LineChartComponent";
 import clsxm from "@/lib/clsxm";
 import { fetchStockInfo } from "@/services/fetchData";
 import { Card, Typography } from "@mui/material";
@@ -6,17 +6,25 @@ import { Card, Typography } from "@mui/material";
 type StockPriceChartCardProps = {
   symbol: string;
 } & React.ComponentPropsWithoutRef<"div">;
+
+interface StockPriceChart {
+  quotations: DataPoint[];
+}
 export default async function StockPriceChartCard({
   symbol,
   className,
   ...rest
 }: StockPriceChartCardProps) {
-  const info = await fetchStockInfo(symbol, "price");
+  const info: StockPriceChart = await fetchStockInfo(symbol, "price");
   return info === null ? (
     <Typography>ไม่พบข้อมูล</Typography>
   ) : (
     <Card className={clsxm(["", className])} {...rest}>
-      <LineChartComponent data={info.quotations} />
+      <LineChartComponent
+        data={info.quotations.filter((q: DataPoint) => {
+          return q.price != null;
+        })}
+      />
     </Card>
   );
 }
